fd975bf5ce8b4233718d0bd64ab9a79eb425e48d
Ali Sayed <70850717+AliEissa74@users.noreply.github.com>
2021-09-29 19:19:13 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -33,7 +33,7 @@ function isInViewport(element) {
     rect.top >= 0 &&
     rect.left >= 0 &&
     rect.bottom <=
-      (window.innerHeight || document.documentElement.clientHeight) &&
+      (1.5 * window.innerHeight || document.documentElement.clientHeight) &&
     rect.right <= (window.innerWidth || document.documentElement.clientWidth)
   );
 }
